0061dc3ef891d003521a4b472107b1cc4a39e765
Ilya Babanov <ilbabanov@gmail.com>
2023-11-25 14:44:46 +0400

    update

--- .lintstagedrc.cjs
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	'*.ts?(x)': [
-		'yarn dlx @biomejs/biome lint --colors=force --verbose --error-on-warnings --log-level=error src ',
-		'yarn dlx @biomejs/biome format src --write'
-	],
-	'*.css': ['yarn dlx stylelint --aei --color --fix --mw=0 -f --rdd --risd --rd  "**/*.css"']
-}
