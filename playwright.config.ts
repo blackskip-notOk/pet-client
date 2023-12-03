096a6b3b21d0b48cf3ec627c74e4c77f4b2a4ff0
Ilya Babanov <ilbabanov@gmail.com>
2023-12-03 12:14:57 +0400

    add a11y test

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,61 +4,58 @@ export default defineConfig({
 	testDir: "./tests",
 	outputDir: "./test-results/",
 	timeout: 30000,
-	// fullyParallel: true,
-	// forbidOnly: !!process.env.CI,
+	fullyParallel: true,
+	forbidOnly: !!process.env.CI,
 	retries: process.env.CI ? 2 : 0,
-	// workers: process.env.CI ? 1 : "50%",
-	// reporter: "html",
+	workers: process.env.CI ? 1 : "50%",
+	reporter: process.env.CI ? "github" : [["html", { open: "on-failure" }]],
 	use: {
 		baseURL: "http://localhost:5173/",
 		trace: "on-first-retry",
-		// screenshot: "only-on-failure",
-		// video: "on-first-retry",
-		// colorScheme: "dark",
-		// locale: "en",
-		// viewport: { width: 1280, height: 720 },
-		// acceptDownloads: false,
-		// ignoreHTTPSErrors: true,
-		// httpCredentials: {
-		// 	username: "user",
-		// 	password: "pass",
-		// },
-		// 	offline: true,
-		// 	actionTimeout: 500,
-		// 	browserName: "chromium",
-		// 	bypassCSP: true,
-		// 	channel: "chrome",
-		// 	headless: true,
-		// 	testIdAttribute: "pw-test-id",
+		screenshot: "only-on-failure",
+		video: "on-first-retry",
+		colorScheme: "dark",
+		locale: "en",
+		viewport: { width: 1280, height: 720 },
+		acceptDownloads: false,
+		ignoreHTTPSErrors: true,
+		httpCredentials: {
+			username: "user",
+			password: "pass",
+		},
+		actionTimeout: 500,
+		browserName: "chromium",
+		bypassCSP: true,
+		channel: "chrome",
+		headless: true,
+		testIdAttribute: "pw-test-id",
 	},
-	// projects: [
-	// 	{
-	// 		name: "chromium",
-	// 		use: { ...devices["Desktop Chrome"] },
-	// 	},
-
-	// 	{
-	// 		name: "firefox",
-	// 		use: { ...devices["Desktop Firefox"] },
-	// 	},
-
-	// 	{
-	// 		name: "webkit",
-	// 		use: { ...devices["Desktop Safari"] },
-	// 	},
-	// ],
-	// webServer: {
-	// 	command: "yarn dev",
-	// 	url: "http://127.0.0.1:5173",
-	// 	reuseExistingServer: !process.env.CI,
-	// },
-	// expect: {
-	// 	timeout: 5000,
-	// 	toHaveScreenshot: {
-	// 		maxDiffPixels: 10,
-	// 	},
-	// 	toMatchSnapshot: {
-	// 		maxDiffPixelRatio: 0.1,
-	// 	},
-	// },
+	projects: [
+		{
+			name: "chromium",
+			use: { ...devices["Desktop Chrome"] },
+		},
+		{
+			name: "firefox",
+			use: { ...devices["Desktop Firefox"] },
+		},
+		{
+			name: "webkit",
+			use: { ...devices["Desktop Safari"] },
+		},
+	],
+	webServer: {
+		command: "yarn dev",
+		url: "http://localhost:5173/",
+		reuseExistingServer: !process.env.CI,
+	},
+	expect: {
+		timeout: 5000,
+		toHaveScreenshot: {
+			maxDiffPixels: 10,
+		},
+		toMatchSnapshot: {
+			maxDiffPixelRatio: 0.1,
+		},
+	},
 });
--- tests/homePage.spec.ts
+++ tests/homePage.spec.ts
@@ -1,4 +1,5 @@
 import { describe } from "node:test";
+import AxeBuilder from "@axe-core/playwright";
 import { expect, test } from "@playwright/test";
 
 test.describe("Home Page @page", () => {
@@ -27,6 +28,18 @@ test.describe("Home Page @page", () => {
 			await page.getByRole("link", { name: /examples/i }).click();
 
 			await expect(page).toHaveURL("/examples");
+		});
+	});
+
+	test.describe("accessibility @a11y", () => {
+		test("should not have any automatically detectable accessibility issues", async ({
+			page,
+		}, testInfo) => {
+			const accessibilityScanResults = await new AxeBuilder({ page })
+				.withTags(["wcag2a", "wcag2aa", "wcag21a", "wcag21aa"])
+				.analyze();
+
+			expect(accessibilityScanResults.violations).toEqual([]);
 		});
 	});
 });
